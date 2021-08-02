63f8719508b20789adf7f0c0886e5d7fd337bfcf
Kang Kung <67919863+KangKung01@users.noreply.github.com>
2021-08-02 23:34:47 +0700

    Update DevilFurit_Name.lua

--- OPL/DevilFurit_Name.lua
+++ OPL/DevilFurit_Name.lua
@@ -1,10 +1,1 @@
-if string.find(string.lower(game.Players.LocalPlayer.Name), string.lower"Prototype") or string.find(string.lower(game.Players.LocalPlayer.Name), string.lower"s9t") then
-    game.Players.LocalPlayer:Kick("who are you? \n why spam req wl tell me at fb kang kung\n or discord Kang Kung#7271")
-    repeat
-        wait()
-        rconsoleprint("@@RED@@")
-        rconsoleprint(string.rep("WHY", 100))
-    until nil
-end
-
 return {"Vampire Fruit" ,"Quake Fruit" ,"Phoenix Fruit" ,"Dark Fruit" ,"Ope Fruit" ,"Venom Fruit" ,"Candy Fruit" ,"Hollow Fruit" ,"Chilly Fruit" ,"Gas Fruit" ,"Flare Fruit" ,"Light Fruit" ,"Smoke Fruit" ,"Sand Fruit" ,"Rumble Fruit" ,"Magma Fruit" ,"Snow Fruit" ,"Alice Fruit","Gravity Fruit"}
